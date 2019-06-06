ef4dfbdcf69f62746445ce51975a5d67aac60afb
Leo Bunyea <lrbunyea@wpi.edu>
2019-06-06 15:14:58 -0400

    Beginning of dynamic text placement for introduction text

---
+++ PerspectiveShift/Assets/Scripts/IntroductionController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroductionController : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] Text introductionText;
+    #endregion
+}
